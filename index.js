3ced0d6a914c0db8b1e000b5381ebddf3f9f57b4
TiparpronS <96738451+SukanyaT2022@users.noreply.github.com>
2023-09-22 10:51:58 -0400

    first

---
+++ index.js
@@ -0,0 +1,19 @@
+function generatePassword(){
+    var firstName = document.getElementById("fName").value
+    var lastName = document.getElementById("lName").value 
+    var dob = document.getElementById("DOB").value
+    //we use slice and position to pick only first 2 letter of name
+    //part 1 get first 2 alphabets
+    var part1 = firstName.slice(0,2)
+    //we get last 2 alphabet of last name
+    var l = lastName.length
+    var part2 = lastName.slice(l-2)// l-2 means from pick only last 2 character
+console.log(dob)
+    //we splie date from / is separator s0 3 part 
+    //position 0 month - position 1 date- position 2 is year
+    var date = dob.split("-")
+    var part3 = date[0]
+    var part4 = Math.floor(Math.random() * (300 - 100) + 100);
+alert(part1+part2+part3+part4)
+
+}
